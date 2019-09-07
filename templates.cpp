855133c652763dde07bb77c8a188b70d2c04debc
Sagar Singla <43108535+sagarsingla14@users.noreply.github.com>
2019-09-07 22:19:42 +0530

    Add files via upload

---
+++ templates.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <class t>
+	t sum(t a,t b)
+	{
+		return a+b;
+	}
+
+int main()
+{
+	cout<<sum(1,2)<<endl;
+	cout<<sum(535.466,318.123)<<endl;
+}
